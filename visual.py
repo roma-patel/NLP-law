82c29d14cca465334920d884af19d75a96d7c0e1
roma-patel <romapatel996@gmail.com>
2017-07-04 12:05:40 -0400

    add visual for cleaned text

---
+++ visual.py
@@ -0,0 +1,40 @@
+import sys
+import codecs
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+from sklearn.manifold import TSNE
+stop_words = ['the', 'at', 'by', 'is', 'has', 'given', 'such', 'on', 'have', 'that', 'this',
+              'who', 'whom', 'whose', 'where', 'how', 'or', 'and', 'be', 'to']
+file_name = 'wvec.txt'
+#f = open('checkkk1.txt', 'w+')
+def main():
+    embeddings_file = 'wvec.txt'
+    wv, vocabulary = load_embeddings(embeddings_file)
+    wv = wv[1:]
+    for i in range(len(wv)):
+        if math.isnan(wv[i]) or math.isinf(wv[i]):
+            wv[i] = 0.0045
+    wv = wv.reshape(-1,1)
+    tsne = TSNE(n_components=2, random_state=0)
+    np.set_printoptions(suppress=True)
+    print'yes'
+    Y = tsne.fit_transform(wv[:50])
+    print 'no'
+    plt.scatter(Y[:, 0], Y[:, 1])
+    for label, x, y in zip(vocabulary, Y[:, 0], Y[:, 1]):
+        plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points')
+    plt.show()
+
+
+def load_embeddings(file_name):
+    with codecs.open(file_name, 'r', 'utf-8') as f_in:
+        vocabulary, wv = zip(*[line.strip().split(' ', 1) for line in
+                               f_in])
+    wv = np.loadtxt(wv)
+    return wv, vocabulary
+
+
+if __name__ == '__main__':
+    main()
